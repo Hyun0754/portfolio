44cbd4c54644b903f63cf2aa3a58fee50ad2a907
Hyun0754 <118324518+Hyun0754@users.noreply.github.com>
2023-02-12 22:50:32 +0900

    0212

--- project002/original/js.js
+++ project002/original/js.js
@@ -2,25 +2,22 @@
 $(document).ready(function(){
         let wd = $(window).width(); // 화면의 가로값
         let ht = $(window).height(); // 화면의 세로값
-        let os = $('.main4').offset().top; //메인4의 오프셋값
-        $('.box').maxwidth(wd)
-        $('.box').height(ht)
-        $('.main4>article').width(wd)
-        let mht = $('.main4').height(ht)
+        let of = $('.main4').offset().top; //메인4의 오프셋값
+        // $('.box').maxwidth(wd)
+        // $('.box').height(ht)
+        // $('.main4>article').width(wd)
         
     $(window).scroll(function(){
         let sc= $(window).scrollTop();
         $('.bg').width(wd)
         $('.bg').height(ht)
-        $('section').width(wd)
-        $('section').height(ht)
         $('.h1').text(sc)
         $('.h2').text(wd)
         $('.h3').text(mht)
-        $('.h4').text(os)
+        $('.h4').text(of)
         })
     //  2뎁스 메뉴 슬라이드
-    $('.nav li').mouseenter(function(){
+    $('.navMenu li').mouseenter(function(){
         let i=$(this).index()
         console.log(i)
         $('.none').stop().slideUp()
@@ -40,46 +37,44 @@ $(document).ready(function(){
     $('header').mouseenter(function(){
         $(this).css({'opacity':1})
         });
-    $(window).scroll(function(){
-        $('.main1Box1').css({'top':600})
-        });
-        ////////////////////////////////////////////////////////////////////////////////
+    $("body").smoothWheel()
+        //////////////////////////////////////////////////////
 });
 
-gsap.registerPlugin(ScrollTrigger);
-let pageContainer = document.querySelector(document);
-/* SMOOTH SCROLL */
-let scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true});
-scroller.on("scroll", ScrollTrigger.update);
-ScrollTrigger.scrollerProxy(pageContainer, {
-  scrollTop(value) {
-    return arguments.length
-      ? scroller.scrollTo(value, 0, 0)
-      : scroller.scroll.instance.scroll.y;
-  },
-});
-////////////////////////////////////
-////////////////////////////////////
-window.addEventListener("load", function () {
-  let pinWrap = document.querySelector(".container");
-  let pinWrapWidth = pinWrap.offsetWidth;
-  let hoz = pinWrapWidth - window.innerWidth;
+// gsap.registerPlugin(ScrollTrigger);
+// let pageContainer = document.querySelector('.wrap');
+// /* SMOOTH SCROLL */
+// let scroller = new LocomotiveScroll({
+//   el: pageContainer,
+//   smooth: true});
+// scroller.on("scroll", ScrollTrigger.update);
+// ScrollTrigger.scrollerProxy(pageContainer, {
+//   scrollTop(value) {
+//     return arguments.length
+//       ? scroller.scrollTo(value, 0, 0)
+//       : scroller.scroll.instance.scroll.y;
+//   },
+// });
+// ////////////////////////////////////
+// ////////////////////////////////////
+// window.addEventListener("load", function () {
+//   let pinWrap = document.querySelector(".container");
+//   let pinWrapWidth = pinWrap.offsetWidth;
+//   let hoz = pinWrapWidth - window.innerWidth;
 
-  // Pinning and horizontal scrolling
+//   // Pinning and horizontal scrolling
 
-  gsap.to(".container", {
-    scrollTrigger: {
-      scroller: pageContainer, //locomotive-scroll
-      scrub: true,
-      trigger: ".main4",
-      pin: true,
-      // anticipatePin: 1,
-      start: "top top",
-      end: pinWrapWidth
-    },
-    x: -hoz,
-    ease: "none"
-  });
-});
+//   gsap.to(".container", {
+//     scrollTrigger: {
+//       scroller: pageContainer, //locomotive-scroll
+//       scrub: true,
+//       trigger: ".main4",
+//       pin: true,
+//       // anticipatePin: 1,
+//       start: "top top",
+//       end: pinWrapWidth
+//     },
+//     x: -hoz,
+//     ease: "none"
+//   });
+// });
